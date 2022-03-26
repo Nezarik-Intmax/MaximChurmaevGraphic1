d87a72a6c1899e786921ebb4538ff772533f332d
User <User@STD08>
2022-03-26 17:06:18 +0500

    Добавьте файлы проекта.

---
+++ MaximChurmaevGraphic1/MaximChurmaevGraphic1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "GL/glut.h";
+#include "GL/glew.h";
+#include "GL/freeglut.h";
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1024, 768);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Tutorial 01");
+}
